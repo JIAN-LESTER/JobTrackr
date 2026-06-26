570d7f5606c77bf51a7b03a8ad687256a4b9dd08
JIAN-LESTER <143403737+JIAN-LESTER@users.noreply.github.com>
2026-06-26 10:17:47 +0800

    Create Reminder Controller with CRUD and Type

---
+++ resources/js/types/Reminder.ts
@@ -0,0 +1,14 @@
+import type { Application } from './Application';
+
+export type Reminder = {
+    reminder_id: number;
+    job_application_id: number;
+    title: string;
+    description: string | null;
+    remind_at: string;
+    is_completed: boolean;
+    created_at: string;
+    updated_at: string;
+    deleted_at?: string | null;
+    job_application?: Application;
+};
