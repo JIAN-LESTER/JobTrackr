8b8bc7a207c429100d392b124e70cea4179a51f3
Jian Lester Tabarno <jianlestertabarno2014@gmail.com>
2026-07-23 15:08:09 +0800

    Apply pagination to activity page

--- resources/js/pages/Timeline.tsx
+++ resources/js/pages/Timeline.tsx
@@ -54,14 +54,20 @@ const statusBadgeClass = (status: string | null) => {
             return 'border-[#2f6f4f]/15 bg-[#dcefe4] text-[#24543d] dark:bg-[#2f6f4f]/25 dark:text-[#b8e6ca]';
         case 'saved':
             return 'border-[#8f6a1f]/15 bg-[#f8edcf] text-[#755516] dark:bg-[#f3c76a]/20 dark:text-[#f8d98a]';
-        case 'assessment':
-            return 'border-[#2f6d7c]/15 bg-[#d9edf1] text-[#245867] dark:bg-[#2f6d7c]/25 dark:text-[#b5e2eb]';
-        case 'interviewing':
+        case 'interview':
+        case 'final_interview':
             return 'border-[#5b4b8a]/15 bg-[#e6e1f2] text-[#4a3d75] dark:bg-[#5b4b8a]/30 dark:text-[#d8cff5]';
         case 'offer':
+        case 'hired':
             return 'border-[#7a5b1c]/15 bg-[#f6e6b8] text-[#654914] dark:bg-[#f3c76a]/25 dark:text-[#ffe7a3]';
         case 'rejected':
+        case 'ghosted':
+        case 'offer_declined':
             return 'border-red-200 bg-red-50 text-red-700 dark:border-red-900/40 dark:bg-red-950/40 dark:text-red-300';
+        case 'closed':
+            return 'border-[#59675e]/20 bg-[#e2e8e3] text-[#435047] dark:border-[#526157]/40 dark:bg-[#27362d] dark:text-[#cbd8cf]';
+        case 'offered_another_position':
+            return 'border-[#2f6d7c]/15 bg-[#d9edf1] text-[#245867] dark:bg-[#2f6d7c]/25 dark:text-[#b5e2eb]';
         case 'added':
         case 'imported':
             return 'border-[#2f6f4f]/15 bg-[#dcefe4] text-[#24543d] dark:bg-[#2f6f4f]/25 dark:text-[#b8e6ca]';
